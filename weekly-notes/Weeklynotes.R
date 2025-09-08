e385ca340564ea42c12f0dcdf1320770ecddb372
nsinghc <nina.carlsen15@gmail.com>
2025-09-08 16:24:07 -0400

    attempt

---
+++ weekly-notes/Weeklynotes.R
@@ -0,0 +1,47 @@
+---
+  title: "Week 1 Notes - Course Introduction"
+date: "2025-09-08"
+---
+  
+  ## Key Concepts Learned
+  - The goal of public policy analytics, as opposed to the private sector, is not necessarily optimization. There are tradeoffs and context that need to be considered
+- Git Vs. Github
+- Git: Version control system to track changes, can be used as a collab tool
+- GitHub: Cloud hosting for git repo; back-up/share/deploy
+- Flow: 
+  - Edit files in RStudio
+- Commit changes with descriptive messages
+- Push to GitHub
+- Portfolio updates automatically
+- Covered GitHub, Quarto, and R/RStudio
+- Repo: folder containing project files
+- Commit: snapshot of work at a point in time
+- Push: sending changes to GitHub cloud
+- Pull: getting latest changes from GitHub cloud (onto local) 
+- Tidyverse 
+- Tibbles vs. Data Frames
+- Tiddles is enhanced data frames
+- shows first 10 rows by default
+- displays column names
+- fits nicely on a screen 
+
+## Coding Techniques
+- R Functions
+- select; filter; mutate; summarize; group_by
+- Quarto Functions
+- How to create and edit a portfolio
+- Text formatting (bold, italic, bold italic, code text, strikethrough, Main Header, Section Header, Subsection Header)
+
+## Questions & Challenges
+- What is the difference between data table and data frame? 
+  - Generally, I need better file management on RStudio
+- Still a little confused about tibbles; are those just better data frames? 
+  - Double-check/review piping 
+
+## Connections to Policy
+- Using open-source tools will be helpful in public work because you avoid licensure/acquisition
+- Possibilities for open-source in the future
+
+## Reflection
+- I really liked the walkthrough of R syntax
+- I will continue to practice ! Excited to learn more 
